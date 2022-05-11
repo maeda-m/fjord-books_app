e2e01df8313b77a0364358e211f5d86285ff372a
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2022-05-11 12:04:22 +0900

    メールアドレスとパスワードを入力してアカウント登録・ログインを実装した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :validatable
   has_one :profile, dependent: :destroy
 end
