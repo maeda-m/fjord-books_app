a567a5707bed0577a318a848b6ed4764db2432e2
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2022-05-12 09:39:05 +0900

    プロフィール編集画面にもアイコンを表示するようにしました

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def render_avatar(avatar, width:, height:)
+  def render_avatar(avatar, width: nil, height: nil)
     return unless avatar.attached?
 
+    size = [width, height]
     alt = User.human_attribute_name(:avatar)
+
+    image = if size.all?(&:nil?)
+              rails_storage_proxy_url(avatar)
+            else
+              avatar.variant(resize_to_limit: size)
+            end
+
     content_tag :figure do
-      image_tag avatar.variant(resize_to_limit: [width, height]), alt: alt
+      image_tag image, alt: alt
     end
   end
 end
