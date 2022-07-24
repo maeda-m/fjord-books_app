d4b9c97119a1a428e24ad86e058f8bf0de9f2116
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2022-07-24 12:01:35 +0900

    カバレッジツールを導入した

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 3.26'
   gem 'selenium-webdriver', '< 4'
+  gem 'simplecov', require: false
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # See: https://github.com/simplecov-ruby/simplecov/issues/718
+  parallelize(workers: :number_of_processors) if ENV['COVERAGE'].blank?
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
